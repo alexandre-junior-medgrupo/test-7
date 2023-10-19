38fd2d53402c7efaf79c2210726916d774a32533
alexandre.junior <alexandre.junior@terceirizados.medgrupo.com.br>
2023-10-19 09:08:33 -0300

    commit

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -1,3 +1,4 @@
+import { angularOutputTarget } from '@stencil/angular-output-target';
 import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 
@@ -37,6 +38,14 @@ export const config: Config = {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
+    angularOutputTarget({
+      componentCorePackage: '@templarios/core',
+      outputType: 'component',
+      directivesProxyFile:
+        '../angular-workspace/projects/component-library/src/lib/stencil-generated/components.ts',
+      directivesArrayFile:
+        '../angular-workspace/projects/component-library/src/lib/stencil-generated/index.ts',
+    }),
   ],
   testing: {
     browserHeadless: 'new',
