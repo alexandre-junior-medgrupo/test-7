fa89dd9c80737c48599c0571775a11575ad47f1c
valdir-junior10 <valdir.conceicaojr@gmail.com>
2023-12-05 14:10:48 +0000

    ajuste scroll ion-segment

--- packages/core/src/components/ionic/segment/segment.stories.ts
+++ packages/core/src/components/ionic/segment/segment.stories.ts
@@ -11,6 +11,18 @@ defineCustomElementSegmentButton();
 const meta: Meta<Components.IonSegment & TpIonSegment> = {
   title: 'components/Ionic/Segment',
   argTypes: {
+    scrollable: {
+      control: 'boolean',
+      description: 'Define se o componente terá barra de scroll.',
+      table: {
+        defaultValue: {
+          summary: false,
+        },
+        type: {
+          summary: 'boolean',
+        },
+      },
+    },
     disabled: {
       control: 'boolean',
       description: 'Define a estilização do estado disabled do componente.',
@@ -32,35 +44,51 @@ type Story = StoryObj<Components.IonSegment & TpIonSegment>;
 
 export const Default: Story = {
   render: ({ ...args }) => html`
-    <ion-segment
-      color="${args.color}"
-      ?disabled="${args.disabled}"
-      value="Tabs1"
-    >
-      <ion-segment-button value="Tabs1">
-        <ion-label>Tabs 1</ion-label>
-      </ion-segment-button>
+    <div class="teste">
+      <ion-segment
+        mode="md"
+        color="${args.color}"
+        ?disabled="${args.disabled}"
+        ?scrollable="${args.scrollable}"
+        value="Tabs1"
+      >
+        <ion-segment-button value="Tabs1">
+          <ion-label>Tabs 1</ion-label>
+        </ion-segment-button>
 
-      <ion-segment-button value="Tabs2">
-        <ion-label>Tabs 2</ion-label>
-      </ion-segment-button>
+        <ion-segment-button value="Tabs2">
+          <ion-label>Tabs 2</ion-label>
+        </ion-segment-button>
 
-      <ion-segment-button value="Tabs3">
-        <ion-label>Tabs 3</ion-label>
-      </ion-segment-button>
+        <ion-segment-button value="Tabs3">
+          <ion-label>Tabs 3</ion-label>
+        </ion-segment-button>
 
-      <ion-segment-button value="Tabs4">
-        <ion-label>Tabs 4</ion-label>
-      </ion-segment-button>
+        <ion-segment-button value="Tabs4">
+          <ion-label>Tabs 4</ion-label>
+        </ion-segment-button>
 
-      <ion-segment-button value="Tabs5">
-        <ion-label>Tabs 5</ion-label>
-      </ion-segment-button>
+        <ion-segment-button value="Tabs5">
+          <ion-label>Tabs 5</ion-label>
+        </ion-segment-button>
 
-      <ion-segment-button value="Tabs6">
-        <ion-label>Tabs 6</ion-label>
-      </ion-segment-button>
-    </ion-segment>
+        <ion-segment-button value="Tabs6">
+          <ion-label>Tabs 6</ion-label>
+        </ion-segment-button>
+
+        <ion-segment-button value="Tabs7">
+          <ion-label>Tabs 7</ion-label>
+        </ion-segment-button>
+
+        <ion-segment-button value="Tabs8">
+          <ion-label>Tabs 8</ion-label>
+        </ion-segment-button>
+
+        <ion-segment-button value="Tabs9">
+          <ion-label>Tabs 9</ion-label>
+        </ion-segment-button>
+      </ion-segment>
+    </div>
   `,
   /* play: async ({ canvasElement }) => {
     const canvas = within(canvasElement);
