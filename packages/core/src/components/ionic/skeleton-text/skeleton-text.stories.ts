b115a17fdd3b8982e23eb9661fd24b5d6f2b62db
alanfedrizzi <alanfedrizzi@gmail.com>
2023-12-06 17:26:29 +0100

    skeleton-test documentação

--- packages/core/src/components/ionic/skeleton-text/skeleton-text.stories.ts
+++ packages/core/src/components/ionic/skeleton-text/skeleton-text.stories.ts
@@ -2,17 +2,18 @@ import { Components } from '@ionic/core';
 import { defineCustomElement } from '@ionic/core/components/ion-skeleton-text';
 import type { Meta, StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
+import { TpIonSkeletonText } from './skeleton-text.type';
 
 defineCustomElement();
 
-const meta: Meta<Components.IonSkeletonText> = {
+const meta: Meta<Components.IonSkeletonText & TpIonSkeletonText> = {
   title: 'components/Ionic/Skeleton Text',
   argTypes: {},
 };
 
 export default meta;
 
-type Story = StoryObj<Components.IonSkeletonText>;
+type Story = StoryObj<Components.IonSkeletonText & TpIonSkeletonText>;
 
 export const Default: Story = {
   render: () => html` <ion-skeleton-text animated></ion-skeleton-text> `,
@@ -20,4 +21,7 @@ export const Default: Story = {
     const canvas = within(canvasElement);
     expect(canvas.getByText(/ion-skeleton-text/gi)).toBeTruthy();
   }, */
+};
+Default.argTypes = {
+  color: { table: { disable: true } },
 };
